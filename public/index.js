7b332c581308883e8bf2b05774392a18860f63ef
Léo PLANUS <48881747+leo29plns@users.noreply.github.com>
2025-12-15 17:07:26 +0100

    chore: :hammer: configure dev env

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
+import { Map as LeafletMap, TileLayer } from 'leaflet';
 import { consoleLog } from '@/scripts/console-log.js';
-import { Map, TileLayer } from 'leaflet';
 
-const map = new Map("map").setView([48.8566, 2.3522], 13);
+const map = new LeafletMap('map').setView([48.8566, 2.3522], 13);
 
 new TileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map);
 
