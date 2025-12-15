a5b901de1f8c4fec9e19c8d493affcf893fc34ea
Léo PLANUS <48881747+leo29plns@users.noreply.github.com>
2025-12-15 17:32:08 +0100

    chore: :technologist: ci cheks

--- public/index.js
+++ public/index.js
@@ -3,6 +3,9 @@ import { consoleLog } from '@/scripts/console-log.js';
 
 const map = new LeafletMap('map').setView([48.8566, 2.3522], 13);
 
-new TileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map);
+new TileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution:
+		'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
 
 consoleLog();
