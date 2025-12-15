6082a2bea7546e49bedaa53909f697e7e6377819
Léo PLANUS <48881747+leo29plns@users.noreply.github.com>
2025-12-15 13:39:36 +0100

    add menu

--- public/index.js
+++ public/index.js
@@ -3,8 +3,6 @@ import { Map, TileLayer } from 'leaflet';
 
 const map = new Map("map").setView([48.8566, 2.3522], 13);
 
-new TileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19
-}).addTo(map);
+new TileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map);
 
 consoleLog();
